0eaea54930776411e2b80e2e7ed221b6d0271521
tatitati <francis.albusac@simplybusiness.co.uk>
2020-12-31 14:48:08 +0000

    more

--- src/test/kotlin/com/example/demo/WebfluxTests.kt
+++ src/test/kotlin/com/example/demo/WebfluxTests.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import org.junit.jupiter.api.Test
 import reactor.core.Disposable
 import reactor.core.publisher.Flux
+import java.time.Duration
 
 class WebfluxTests {
 
@@ -31,4 +32,38 @@ class WebfluxTests {
         // bbbb
         // completed
     }
+
+    @Test
+    fun `basic test3`(){
+        val publisher: Flux<String> = Flux.just("aaa", "bbbb", "cccc", "dddd", "eeee")
+        val subscription: Disposable = publisher.subscribe(
+                { item: String -> println(item)},
+                {e -> println(e)},
+                {println("completed")},
+                {sub -> sub.request(3)}
+        )
+        //OUTPUT:
+        // aaa
+        // bbbb
+        // cccc
+    }
+
+    @Test
+    fun `inifinite flux1`(){
+        val publisher: Flux<Long> = Flux.interval(Duration.ofSeconds(2))
+        val subscription: Disposable = publisher.subscribe(
+                { item: Long -> println(item)}
+        )
+
+        Thread.sleep(10000)
+
+        //OUTPUT:
+        // 0
+        // 1
+        // 2
+        // 3
+        // ...
+    }
+
+
 }
