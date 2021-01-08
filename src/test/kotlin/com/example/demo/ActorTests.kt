13577603aabb1cab18755ab5089d61d763a10883
tatitati <francis.albusac@simplybusiness.co.uk>
2021-01-08 18:54:47 +0000

    first actor added

---
+++ src/test/kotlin/com/example/demo/ActorTests.kt
@@ -0,0 +1,43 @@
+package com.example.demo
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import org.junit.jupiter.api.Test
+
+class ActorTests {
+
+    open class CounterMsg(){}
+    object MsgIncCounter: CounterMsg() // one-way message to increment counter
+    class MsgGetCounter(val response: CompletableDeferred<Int>) : CounterMsg() // a request with reply
+
+    fun CoroutineScope.counterActor() = actor<CounterMsg> {
+        var counter = 0 // actor state
+        for (msg in channel) { // iterate over incoming messages
+            when (msg) {
+                is MsgIncCounter -> counter++
+                is MsgGetCounter -> msg.response.complete(counter)
+            }
+        }
+    }
+
+    @Test
+    fun `actor`(){
+        runBlocking<Unit> {
+            val actorCounter = counterActor() // create the actor
+            withContext(Dispatchers.Default) {
+                    actorCounter.send(MsgIncCounter)
+                    actorCounter.send(MsgIncCounter)
+                    actorCounter.send(MsgIncCounter)
+            }
+
+            val response = CompletableDeferred<Int>()
+            actorCounter.send(MsgGetCounter(response))
+            println("Counter = ${response.await()}")
+            actorCounter.close() // shutdown the actor
+
+            // OUTPUT:
+            // Counter = 3
+        }
+    }
+}
