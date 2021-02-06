c5d91edbbbd88b10501475e4117f6828d7456d96
tatitati <francis.albusac@simplybusiness.co.uk>
2021-02-06 16:59:39 +0000

    added simple actor to increment and retrieve values

--- src/test/kotlin/com/example/demo/actor/myactor/MyActor2.kt
+++ src/test/kotlin/com/example/demo/actor/myactor/MyActor2.kt
@@ -10,34 +10,48 @@ import kotlinx.coroutines.*
 class CounterRegistry : CoroutineScope {
     private val job = Job()
     override val coroutineContext = Dispatchers.Unconfined + job
-    private val actor = myactor2()
+    private val actor = myactor()
+    var number = 0
+    fun CoroutineScope.myactor() = actor<Msg> {
+        for (message in channel) {
+            when (message) {
+                is MsgIncrement -> {
+                    number++
+                }
+            }
+        }
+    }
 
     suspend fun inc() {
         actor.send(MsgIncrement())
     }
 
-    suspend fun getNumber() {
-        val result = CompletableDeferred<Int>()
-        actor.send(MsgRetrieve(result))
-        val finalValue = result.await()
-        println(finalValue)
+    fun getNumber(){
+        println(number)
     }
+
+    // suspend fun getNumber() {
+    //     val result = CompletableDeferred<Int>()
+    //     actor.send(MsgRetrieve(result))
+    //     val finalValue = result.await()
+    //     println(finalValue)
+    // }
 }
 
-private fun CoroutineScope.myactor2() = actor<Msg> {
-    var number: Int = 0
-
-    for (message in channel) {
-        when (message) {
-            is MsgIncrement -> {
-                number++
-            }
-            is MsgRetrieve -> {
-                message.result.complete(number)
-            }
-        }
-    }
-}
+// private fun CoroutineScope.myactor2() = actor<Msg> {
+//     var number: Int = 0
+//
+//     for (message in channel) {
+//         when (message) {
+//             is MsgIncrement -> {
+//                 number++
+//             }
+//             is MsgRetrieve -> {
+//                 message.result.complete(number)
+//             }
+//         }
+//     }
+// }
 
 open class Msg
 class MsgIncrement: Msg()
